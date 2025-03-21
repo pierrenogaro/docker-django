42394d735d547ba04e0904bd565884ae5df77ea9
Pierre <pierre.nogaro@gmail.com>
2025-03-21 15:23:39 +0100

    postgres (test)

--- website/urls.py
+++ website/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home_page, name='home'),
     path('autre', views.other_page, name='other_page'),
+    path('products/', views.product_list, name='product_list'),
     path('create/', views.product_create, name='product_create'),
     path('update/<int:pk>/', views.product_update, name='product_update'),
     path('delete/<int:pk>/', views.product_delete, name='product_delete'),
