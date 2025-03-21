1a4519360e18706fcf55532a93df41b4e6d59940
Pierre <pierre.nogaro@gmail.com>
2025-03-21 14:54:03 +0100

    crud

--- website/urls.py
+++ website/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home_page, name='home'),
     path('autre', views.other_page, name='other_page'),
+    path('create/', views.product_create, name='product_create'),
+    path('update/<int:pk>/', views.product_update, name='product_update'),
+    path('delete/<int:pk>/', views.product_delete, name='product_delete'),
 ]
--- website/views.py
+++ website/views.py
@@ -4,3 +4,43 @@ def home_page(request):
 
 def other_page(request):
     return render(request, 'website/other_page.html')
+
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Product
+from .forms import ProductForm
+
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'website/products/product_list.html', {'products': products})
+
+
+def product_create(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm()
+    return render(request, 'website/products/product_form.html', {'form': form})
+
+
+def product_update(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'website/products/product_form.html', {'form': form})
+
+
+def product_delete(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        product.delete()
+        return redirect('product_list')
+    return render(request, 'website/products/product_confirm_delete.html', {'product': product})
